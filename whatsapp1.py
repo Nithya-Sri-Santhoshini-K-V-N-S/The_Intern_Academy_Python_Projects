3bff416a95057eb7ce4913fbb19840f304045555
NITHYA SRI SANTHOSHINI K V N S <90061603+Nithya-Sri-Santhoshini-K-V-N-S@users.noreply.github.com>
2021-11-06 20:35:38 +0530

    Delete whatsapp1.py

--- whatsapp1.py
+++
@@ -1,9 +0,0 @@
-import pywhatkit as kit
-
-try:
-    kit.sendwhatmsg("+91xxxxxxxxxx","Happy Birthday! I hope you have a great day.",23,27)
-    kit.sendwhatmsg("+91xxxxxxxxxx","Happy Diwali!",23,28)
-    print("Whatsapp Message sent successfully")
-
-except:
-    print("Error! Whatsapp Message not sent")
